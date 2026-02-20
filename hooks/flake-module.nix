4324738ba2c0dc4c6969db48363f44dca268aafa
Aleksander Heintz <alxandr@alxandr.me>
2026-02-20 16:27:24 +0100

    hook priority

--- hooks/flake-module.nix
+++ hooks/flake-module.nix
@@ -13,35 +13,53 @@
 
     in
     {
-      # Some hooks cannot run in a build environment
-      pre-commit.check.enable = false;
-      pre-commit.settings.package = prek;
-      pre-commit.settings.hooks = {
-        flake-checker.enable = true;
-        nixfmt.enable = true;
-      };
+      # options = {
 
-      devshells.default =
-        { pkgs, ... }:
-        {
-          commands = [
-            {
-              name = "prek";
-              help = "run pre-commit hooks";
-              category = "lint";
-              command = ''
-                ${prek}/bin/prek run
-              '';
-            }
-          ];
+      # };
 
-          devshell = {
-            # packages = sysCfg.pre-commit.settings.enabledPackages;
-            startup.install-git-hooks.text = ''
-              # pre-commit installation script
-              ${config.pre-commit.installationScript}
-            '';
+      config = {
+        pre-commit.settings.hookModule =
+          { config, ... }:
+          {
+            options.priority = lib.mkOption {
+              type = lib.types.int;
+              default = 0;
+              description = "Priority of the hook";
+            };
+
+            config.raw.priority = config.priority;
           };
+
+        # Some hooks cannot run in a build environment
+        pre-commit.check.enable = false;
+        pre-commit.settings.package = prek;
+        pre-commit.settings.hooks = {
+          flake-checker.enable = true;
+          nixfmt.enable = true;
         };
+
+        devshells.default =
+          { ... }:
+          {
+            commands = [
+              {
+                name = "prek";
+                help = "run pre-commit hooks";
+                category = "lint";
+                command = ''
+                  ${prek}/bin/prek
+                '';
+              }
+            ];
+
+            devshell = {
+              # packages = sysCfg.pre-commit.settings.enabledPackages;
+              startup.install-git-hooks.text = ''
+                # pre-commit installation script
+                ${config.pre-commit.installationScript}
+              '';
+            };
+          };
+      };
     };
 }
